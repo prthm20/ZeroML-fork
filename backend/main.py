d857554683f2447a198902da0390e19f584c2cd9
prthm20 <aprathamm@gmail.com>
2025-09-12 03:49:51 +0530

    upload feature

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,16 @@
+
 from fastapi import FastAPI
 from app.routes.routes import router as article_router
 from fastapi.middleware.cors import CORSMiddleware
+from app.logging.logging_config import setup_logger
+    
+# Setup logger for this module
+logger = setup_logger(__name__)
 
 app = FastAPI(
-    title="ZeroML backend",
+    title="ZeroMl",
     version="1.0.0",
-    description=("An API to handle all the complex logic for ZeroML"),
+    description=("Flow builder for machin learning"),
 )
 
 app.add_middleware(
@@ -16,12 +21,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(article_router, tags=["Articles"])
+app.include_router(article_router, prefix="/api", tags=["Articles"])
 
 if __name__ == "__main__":
     import uvicorn
     import os
 
     port = int(os.environ.get("PORT", 7860))
-    print("Server is live at http://localhost:7860!")
+    logger.info(f" Server is running on http://localhost:{port}")
     uvicorn.run(app, host="0.0.0.0", port=port)
